6f85cdae55b5ec8a4b990a6c917bcf2097f410b9
Francesco Strazzullo <me@francescostrazzullo.info>
2015-06-26 22:03:42 +0200

    imported google visualization api

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,5 @@
 import React from "react";
 
-export default React.render(<div>Hello World!</div>,document.getElementById('wrapper'));
+export default (function(){
+	React.render(<div>Hello World!</div>,document.getElementById('wrapper'));
+})();
