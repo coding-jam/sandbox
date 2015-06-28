03179842c043241e067e18e00b8ef75b07f9a0c1
Francesco Strazzullo <me@francescostrazzullo.info>
2015-06-28 10:58:02 +0200

    first implementation of geochart

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,9 +1,31 @@
 import React from "react";
 
 export default class Map extends React.Component{
+
+	componentDidMount() {
+
+		var data = google.visualization.arrayToDataTable([
+          ['Country', 'Popularity'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700]
+        ]);
+
+        var options = {};
+
+        var chart = new google.visualization.GeoChart(React.findDOMNode(this.refs.chart));
+
+        chart.draw(data, options);
+	}
+
 	render() {
 	    return (
-			<div>Map!</div>
+	    	<div>
+				<div ref="chart"></div>
+			</div>
 	    );
 	}
 }
