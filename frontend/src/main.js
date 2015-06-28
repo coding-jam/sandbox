8a07e62d8529ee1df11e6e63bdc76d15c4ffdfdd
Francesco Strazzullo <me@francescostrazzullo.info>
2015-06-28 10:36:35 +0200

    created Map component stub

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import React from "react";
+import Map from "src/components/Map";
 
 export default (function(){
-	React.render(<div>Hello World!</div>,document.getElementById('wrapper'));
+	React.render(<div>Hello World!<Map></Map></div>,document.getElementById('wrapper'));
 })();
