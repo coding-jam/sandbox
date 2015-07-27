8a2fda4ed5d24134706edce306f3c05f8ca083d2
andrea.como <andrea.como@gmail.com>
2015-07-28 01:23:09 +0200

    Added tests on repo details retrival

--- backend/data-collector.js
+++ backend/data-collector.js
@@ -1,11 +1,17 @@
 var ghHttp = require('./gh-http');
 var Q = require('q');
-var fs = require('fs')
+var fs = require('fs');
+var _ = require("underscore");
 
 var collector = {
 
+    data: {
+        folder: __dirname + '/data/',
+        users: 'it_users.json'
+    },
+
     options: {
-        secrets: 'client_id=51f5e69c42514ef98707&client_secret=a57c2fabcf4f3c655d9381b2c56134f76fa8fdc5',
+        secrets: ghHttp.secrets,
         resourceTemplate: 'https://api.github.com/search/users?q=created:%22{range}%22%20location:italy%20type:user&sort=joined&per_page=100&{secret}',
         rangeTemplate: [
             '{year}-01-01%20..%20{year}-03-31',
@@ -17,6 +23,7 @@ var collector = {
 
     collectUsers: function () {
 
+        var deferred = Q.defer();
         var year = 2008;
 
         var allPromises = [];
@@ -28,17 +35,20 @@ var collector = {
         }
 
         Q.all(allPromises).then(function (data) {
-            saveUsers(data);
+            saveUsers(data, deferred);
         }, function (error) {
             console.error('ERROR!! ' + error.message);
+            deferred.reject(error);
         });
+
+        return deferred.promise;
 
         function executeRequest(i) {
             var url = collector.options.resourceTemplate.replace(/\{secret\}/g, collector.options.secrets);
             return ghHttp.getWithLimit(createRange(url, year, i), true)
                 .then(function (resp) {
                     return resp.body;
-                }, function(err) {
+                }, function (err) {
                     console.error(err);
                 });
 
@@ -49,30 +59,117 @@ var collector = {
             }
         }
 
-        function saveUsers(data) {
+        function saveUsers(data, deferred) {
             var result = {
                 total_count: 0,
                 items: []
             };
 
-            data.forEach(function (json, i) {
-                var jsonObj = JSON.parse(json);
+            data.forEach(function (jsonObj, i) {
                 if (jsonObj.total_count != undefined) {
                     result.total_count = result.total_count + jsonObj.total_count;
                     result.items = result.items.concat(jsonObj.items);
                 } else {
-                    console.error('Found json: ' + json)
+                    console.error('Found json: ' + jsonObj)
                 }
             });
-            var fileName = 'it_users.json';
-            fs.writeFile(__dirname + '/data/' + fileName, JSON.stringify(result), function (err) {
+            fs.writeFile(collector.data.folder + collector.data.users, JSON.stringify(result), function (err) {
                 if (err) {
                     console.error(err);
                 }
 
-                console.log(fileName + ' saved');
+                console.log(collector.data.users + ' saved');
+                deferred.resolve(result);
             });
             console.log('Total users found ' + result.total_count);
+        }
+    },
+
+    collectUserDetails: function () {
+
+        var deferred = Q.defer();
+        fs.exists(collector.data.folder + collector.data.users, function (exists) {
+            if (!exists) {
+                collector.collectUsers()
+                    .then(function (users) {
+                        addDetails(users, deferred);
+                    }, function(err) {
+                        deferred.reject(err);
+                    });
+            } else {
+                fs.readFile(collector.data.folder + collector.data.users, 'utf8', function (err, data) {
+                    if (err) {
+                        deferred.reject(err);
+                    }
+                    var users = JSON.parse(data);
+                    addDetails(users, deferred);
+                });
+            }
+        });
+
+        return deferred.promise;
+
+
+        function addDetails(users, deferred) {
+
+            var promises = [];
+
+            users.items.forEach(function(user, i) {
+                var deferredLoop = Q.defer();
+                //if (user.url == 'https://api.github.com/users/mcollina') {
+                    ghHttp.getWithLimit(user.url + '?' + collector.options.secrets, false)
+                        .then(function(resp) {
+                            return _.extend(user, resp.body);
+                        })
+                        .then(function(user) {
+                            return addRepoDetails(user);
+                        })
+                        .then(function(user) {
+                            deferredLoop.resolve(user);
+                        })
+                        .catch(function(err) {
+                            throw err;
+                        });
+                    promises.push(deferredLoop.promise);
+                //}
+            });
+
+            Q.all(promises).then(function() {
+                fs.writeFile(collector.data.folder + collector.data.users + '.full', JSON.stringify(users), function (err) {
+                    if (err) {
+                        console.error(err);
+                    }
+
+                    console.log(collector.data.users + ' updated');
+                    deferred.resolve();
+                });
+            });
+
+            function addRepoDetails(user) {
+                var promises = [];
+                for (var i = 1; i <= (Math.floor(user.public_repos / 100) + 1); i++) {
+                    (function() {
+                        var pagedDeferred = Q.defer();
+                        ghHttp.getWithLimit(user.repos_url + '?' + collector.options.secrets + '&per_page=100&page=' + i, false)
+                            .then(function (resp) {
+                                var languages = _.pluck(resp.body, 'language');
+                                pagedDeferred.resolve(languages);
+                            })
+                            .catch(function (err) {
+                                console.error(err);
+                                pagedDeferred.reject(err);
+                            });
+                        promises.push(pagedDeferred.promise);
+                    })();
+                }
+                return Q.all(promises).then(function(languages) {
+                    user.languages = _.chain(languages).flatten().unique().compact().value();
+                    return user;
+                }, function(err) {
+                    console.error(err);
+                    return user;
+                });
+            }
         }
     }
 }
--- backend/gh-http.js
+++ backend/gh-http.js
@@ -1,25 +1,34 @@
 var request = require("request");
+var requestSync = require('sync-request');
 var Q = require("q");
 var _ = require("underscore");
 
 var ghHttp = {
 
+    secrets: 'client_id=51f5e69c42514ef98707&client_secret=a57c2fabcf4f3c655d9381b2c56134f76fa8fdc5',
+
     rateLimit: {
         requests: {
             limit: 5000,
             interval: 60 * 60 * 1000,
-            activeRequests: 0
+            resetTime: 0
         },
         search: {
             limit: 30,
             interval: 1 * 60 * 1000,
-            activeRequests: 0
-        }
+            resetTime: 0
+        },
+        queue: {
+            limit: 50,
+            interval: 15 * 1000
+        },
+        activeRequests: 0,
+
     },
 
     options: {
         method: "GET",
-        timeout: 10000,
+        timeout: 1000000,
         followRedirect: true,
         maxRedirects: 10,
         headers: {
@@ -46,18 +55,27 @@ var ghHttp = {
 
     getWithLimit: function(url, isSearch) {
 
-        if (isSearch) {
-            return delayRequest(ghHttp.rateLimit.search);
+        if (ghHttp.rateLimit.activeRequests < ghHttp.rateLimit.queue.limit) {
+            if (isSearch) {
+                return delayRequest(ghHttp.rateLimit.search);
+            } else {
+                return delayRequest(ghHttp.rateLimit.requests);
+            }
         } else {
-            return delayRequest(ghHttp.rateLimit.requests);
+            console.info('Queue limit of ' + ghHttp.rateLimit.queue.limit + ' exceeded, add delay of ' + (ghHttp.rateLimit.queue.interval / 1000) + ' seconds');
+            return Q.delay(ghHttp.rateLimit.queue.interval).then(function() {
+                return ghHttp.getWithLimit(url, isSearch);
+            });
         }
 
         function delayRequest(limitParams) {
-            if (limitParams.activeRequests < limitParams.limit) {
+            if (ghHttp.rateLimit.activeRequests < limitParams.limit || limitParams.resetTime < Math.floor(Date.now() / 1000)) {
+                console.info('Execute request now: limit set to ' + limitParams.limit);
                 return executeRequest(limitParams);
             } else {
-                console.log('Apply request delay of ' + (limitParams.interval / 1000 / 60) + ' minute/s');
-                return Q.delay(limitParams.interval).then(function() {
+                var interval = limitParams.resetTime - Math.floor(Date.now() / 1000);
+                console.log('Apply request delay of ' + (interval) + ' seconds');
+                return Q.delay(interval * 1000).then(function() {
                     return delayRequest(limitParams);
                 });
             }
@@ -66,24 +84,51 @@ var ghHttp = {
         function executeRequest(limitParams) {
             var options = _.clone(ghHttp.options);
             options.uri = url;
-            limitParams.activeRequests++
+            ghHttp.rateLimit.activeRequests++
 
-            console.log('Start new request. Active requests: ' + limitParams.activeRequests);
+            console.log('Start new request. Active requests: ' + ghHttp.rateLimit.activeRequests);
 
             var deferred = Q.defer();
             request(options, function (error, response, body) {
-                limitParams.activeRequests--
+                ghHttp.rateLimit.activeRequests--
                 if (error) {
-                    console.log('Rejected. Active requests: ' + limitParams.activeRequests);
+                    console.log('Rejected. Active requests: ' + ghHttp.rateLimit.activeRequests);
                     deferred.reject(error);
                 } else {
-                    console.log('Resolved. Active requests: ' + limitParams.activeRequests);
-                    deferred.resolve({response: response, body: body});
+                    ghHttp.updateLimits(response, limitParams);
+                    console.log('Resolved. Active requests: ' + ghHttp.rateLimit.activeRequests);
+                    var responseObj = {response: response, body: body};
+                    if (response.headers['content-type'].match('application/json')) {
+                        responseObj.body = JSON.parse(body);
+                    }
+                    deferred.resolve(responseObj);
                 }
             });
             return deferred.promise;
         }
+    },
+
+    updateLimits: function (response, limitParams) {
+        limitParams.limit = response.headers['x-ratelimit-remaining'];
+        limitParams.resetTime = response.headers['x-ratelimit-reset'];
+    },
+
+    setupApiLimits: function() {
+
+        var headers = _.extend({}, ghHttp.options.headers);
+        headers['If-None-Match'] = 'W/"bbb234d54455c9ae9fe93d66e6c041a1"';
+        getLimitsFor(ghHttp.rateLimit.requests, 'https://api.github.com/users/cosenonjaviste?');
+        getLimitsFor(ghHttp.rateLimit.search, 'https://api.github.com/search/users?q=cosenonjaviste%20in:name%20type:user&');
+
+        function getLimitsFor(limitParams, url) {
+            var res = requestSync('GET', url + ghHttp.secrets, {
+                'headers': headers
+            });
+            ghHttp.updateLimits(res, limitParams);
+        };
     }
 }
+
+ghHttp.setupApiLimits();
 
 module.exports = ghHttp;
