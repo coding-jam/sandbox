7b371cf06d65579a837fefa2daa5f9221325ad77
andrea.como <andrea.como@gmail.com>
2015-07-31 00:19:49 +0200

    Just first location taken in consideration

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,16 +10,15 @@ router.get('/:regione', function (req, res) {
     var respData = {};
     _.keys(locationData).forEach(function (key) {
 
-        var found = _.find(locationData[key].results, function (result) {
-            var filtered = _.find(result.address_components, function (address) {
+        if (locationData[key].length > 0) {
+            var found = _.find(locationData[key][0].address_components, function (address) {
                 return _.contains(address.types, 'administrative_area_level_1')
                     && address.short_name && address.short_name.toLowerCase() == req.params.regione.toLowerCase();
             });
-            return !_.isEmpty(filtered);
-        });
 
-        if (found) {
-            respData[key] = locationData[key];
+            if (found) {
+                respData[key] = locationData[key];
+            }
         }
     });
     res.json(respData);
