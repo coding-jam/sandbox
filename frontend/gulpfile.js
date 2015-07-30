a081382681c8ccecded21abf1173e1ba133d4bf8
andrea.como <andrea.como@gmail.com>
2015-07-31 00:51:49 +0200

    Added css to gulp build

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,26 +3,29 @@ var connect = require('gulp-connect');
 var rename = require("gulp-rename");
 var del = require('del');
 var shell = require('gulp-shell');
- 
-gulp.task('serve', function() {
-  connect.server({
-    livereload: true,
-    port:8000
-  });
+
+gulp.task('serve', function () {
+    connect.server({
+        livereload: true,
+        port: 8000
+    });
 });
 
-gulp.task('build',function(){
-	del(['./../build/**/*'],{force:true});
+gulp.task('build', function () {
+    del(['./../build/**/*'], {force: true});
 
-	gulp.src('./index_prod.html')
-	.pipe(rename("index.html"))
-	.pipe(shell(['jspm bundle-sfx src/main ../build/app.js']))
-	.pipe(gulp.dest('./../build'));
+    gulp.src('./index_prod.html')
+        .pipe(rename("index.html"))
+        .pipe(shell(['jspm bundle-sfx src/main ../build/app.js']))
+        .pipe(gulp.dest('./../build'));
+
+    gulp.src('./css/**')
+        .pipe(gulp.dest('./../build/css'));
 });
 
-gulp.task('serve-build',['build'],function(){
-  	connect.server({
-  		root:'../build',
-		port:8001
-  	});
+gulp.task('serve-build', ['build'], function () {
+    connect.server({
+        root: '../build',
+        port: 8001
+    });
 });
