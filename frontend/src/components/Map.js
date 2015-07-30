8ccbbcf947cb00479555debe004c26b57cd10cb9
Francesco Strazzullo <me@francescostrazzullo.info>
2015-07-30 22:11:13 +0200

    first draft of gmap integration

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,61 +1,32 @@
 import React from "react";
 import jQuery from "jquery";
 import _ from "underscore";
-import HighmapsLib from "highslide-software/highmaps-release/highmaps";
-import mapData from "src/map/italy";
 
 export default class Map extends React.Component {
 	constructor(props) {
-	    super(props);
-	    
-	    this.draw = this._draw.bind(this);
-	}
-
-	_draw(data) {
-
-		jQuery(React.findDOMNode(this.refs.chart)).highcharts('Map', {
-			chart:{margin: 0},
-			mapNavigation: {
-				enabled: true,
-				buttonOptions: {
-					verticalAlign: 'bottom'
-				}
-			},
-
-			title : {
-         	   text : ''
-        	},
-
-			colorAxis: {
-	            min: 0
-	        },
-
-			series: [{
-				data: data,
-				mapData: mapData,
-				joinBy: 'hc-key',
-				states: {
-					hover: {
-						color: '#BADA55'
-					}
-				}
-			}]
-		});
+		super(props);
 	}
 
 	componentDidMount() {
+		var mapOptions = {
+			center: {
+				lat: -34.397,
+				lng: 150.644
+			},
+			zoom: 8
+		};
 
-		var self = this;
-
-		jQuery.ajax({
-			url: "http://www.json-generator.com/api/json/get/cgtKuLyKdK?indent=2",
-		}).then(function(results) {
-			self.draw(results);
-		});
-
+		var map = new google.maps.Map(React.findDOMNode(this.refs.chart), mapOptions);
 	}
 
 	render() {
-		return (<div><div ref="chart"></div></div>);
+		return (
+			<div>
+				<div 
+					className="Map"
+					ref="chart">
+				</div>
+			</div>
+		);
 	}
 }
