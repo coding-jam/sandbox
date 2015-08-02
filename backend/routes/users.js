c1da2acdaf997d908721d99fb7bb452f57a622fa
andrea.como <andrea.como@gmail.com>
2015-08-02 15:13:24 +0200

    Regions and users count

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,13 @@ var userAdapter = require(__dirname + "/../services/users-adapter")
 
 var router = express.Router();
 
+router.get('/', function (req, res) {
+    userAdapter.getUsersPerRegione()
+        .then(function(users) {
+            res.json(users);
+        });
+});
+
 router.get('/:regione', function (req, res) {
     userAdapter.getByRegione(req.params.regione)
         .then(function(users) {
--- backend/services/users-adapter.js
+++ backend/services/users-adapter.js
@@ -1,4 +1,5 @@
 var _ = require("underscore");
+var Q = require('q');
 var userDs = require(__dirname + "/users-datasource");
 var locationDs = require(__dirname + "/locations-datasource");
 
@@ -23,6 +24,37 @@ var userAdapter = {
                     items: filtered
                 }
             });
+    },
+
+    getUsersPerRegione: function() {
+        var result = {
+            locations: []
+        };
+        return locationDs.findRegioni()
+            .then(function(regioni) {
+                var promises = [];
+                regioni.forEach(function(regione) {
+                    var deferredLoop = Q.defer();
+                    userAdapter.getByRegione(regione.toLowerCase())
+                        .then(function(users) {
+                            deferredLoop.resolve({
+                                name: regione,
+                                url: 'TODO',
+                                users: users.total_count
+                            });
+                        })
+                        .catch(function(err) {
+                            deferredLoop.reject(err);
+                        });
+                    promises.push(deferredLoop.promise);
+                });
+                return Q.all(promises);
+            })
+            .then(function(usersPerRegions) {
+                result.locations = usersPerRegions
+                return result;
+            });
+
     }
 
 };
