b93a88fbc79dc4a41c094f6c2a319ce8fc5c55af
andrea.como <andrea.como@gmail.com>
2015-08-03 01:09:36 +0200

    Reformatted information on languages

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,8 +8,17 @@ var router = express.Router();
 router.get('/', function (req, res) {
     userAdapter.getUsersPerRegione(api.getApiPath() + api.usersPath)
         .then(function(users) {
-            res.json(users);
+            var resBody = _.extend({}, users);
+            addLanguagesInfo(resBody);
+            res.json(resBody);
         });
+
+    function addLanguagesInfo(resBody) {
+        resBody.languages = api.getApiPath() + api.languagesPath;
+        resBody.locations.forEach(function(location) {
+            location.languages = location.url.replace(api.usersPath, api.languagesPath);
+        });
+    }
 });
 
 router.get('/:regione', function (req, res) {
