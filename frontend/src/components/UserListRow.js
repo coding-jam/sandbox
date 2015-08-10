f7bd782685edec4906070b272ff94f779c8bdf26
Francesco Strazzullo <me@francescostrazzullo.info>
2015-08-10 23:38:24 +0200

    cosmetics

--- frontend/src/components/UserListRow.js
+++ frontend/src/components/UserListRow.js
@@ -28,48 +28,23 @@ export default class UserListRow extends React.Component {
 				<td>
 					<dl>
 						<dt>
-							Username
-						</dt>
-						<dd>
-							{this.props.user.login}
-						</dd>
-						<dt className={this.props.user.name ? 'show' : 'hidden'}>
-							Nome
+							<a href={this.props.user.html_url} target="_blank">
+								<h3 className="NoPadding">{this.props.user.login}</h3>
+							</a>
 						</dt>
 						<dd className={this.props.user.name ? 'show' : 'hidden'}>
 							{this.props.user.name}
 						</dd>
-						<dt className={this.props.user.blog ? 'show' : 'hidden'}>
-							Blog
-						</dt>
 						<dd className={this.props.user.blog ? 'show' : 'hidden'}>
 							<a target="_blank" href={this.props.user.blog}>{this.props.user.blog}</a>
 						</dd>
-						<dt className={this.props.user.bio ? 'show' : 'hidden'}>
-							Bio
-						</dt>
-						<dd className={this.props.user.bio ? 'show' : 'hidden'}>
-							{this.props.user.bio}
-						</dd>
-						<dt>
-							Followers
-						</dt>
-						<dd>
-							{this.props.user.followers}
-						</dd>
-						<dt>
-							Repository
-						</dt>
-						<dd>
-							{this.props.user.public_repos}
-						</dd>
-						<dt className={this.props.user.languages.length ? 'show' : 'hidden'}>
-							Linguaggi
-						</dt>
 						<dd className={this.props.user.languages.length ? 'show' : 'hidden'}>
 							<ul className="list-inline">
 								{this.renderLanguages(this.props.user.languages)}
 							</ul>
+						</dd>
+						<dd className={this.props.user.location ? 'show' : 'hidden'}>
+							<i>{this.props.user.location}</i>
 						</dd>
 					</dl>
 				</td>
