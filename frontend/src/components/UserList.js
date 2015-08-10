514cf0c1a86dffc7dedba8bd906da6cc9bb4e9d0
Francesco Strazzullo <me@francescostrazzullo.info>
2015-08-10 15:37:12 +0200

    added user list

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import * as bootstrap from "react-bootstrap";
 import Store from "src/store/UserListStore";
+import _ from "lodash";
 
 var Modal = bootstrap.Modal;
+var Table = bootstrap.Table;
 
 export default class UserList extends React.Component {
 	constructor(props) {
@@ -12,8 +14,9 @@ export default class UserList extends React.Component {
 		};
 
 		this.close = this._close.bind(this);
-		this.open = this._open.bind(this);
 		this.listener = this._listener.bind(this);
+		this.extendState = this._extendState.bind(this);
+		this.renderRows = this._renderRows.bind(this);
 	}
 
 	componentDidMount() {
@@ -24,31 +27,64 @@ export default class UserList extends React.Component {
 		Store.removeChangeListener(this.listener);
 	}
 
+	_extendState(data){
+		this.setState(_.extend(this.state, data));
+	}
+
 	_listener() {
-		this.open();
+		var data = _.extend(_.clone(Store.getData()),{showModal:true});
+		this.extendState(data);
 	}
 
 	_close(){
-		this.setState({
-			showModal:false
-		});
+		this.extendState({showModal:false});
 	};
 
-	_open(){
-		this.setState({
-			showModal:true
-		});
-	};
+	_renderRows(){
+		if(!this.state.users){
+			return null;
+		}
+
+		var items = this.state.users.map(function(user,i) {
+			return (
+				<tr key={user.id}>
+					<td><img width="50" src={user.avatar_url}></img></td>
+					<td>{user.name}</td>
+					<td>{user.login}</td>
+					<td><a href={"mailto://" + user.email}>{user.email}</a></td>
+					<td><a target="_blank" href={user.blog}>{user.blog}</a></td>
+				</tr>
+			);
+		},this);
+
+		return items;
+	}
 
 	render() {
+
+		var rows = this.renderRows();
+
 		return ( 
 			<div>
-				<Modal show={this.state.showModal} onHide={this.close}>
+				<Modal bsSize='large' show={this.state.showModal} onHide={this.close}>
 		          <Modal.Header closeButton>
-		            <Modal.Title>Titolo</Modal.Title>
+		            <Modal.Title>{this.state.location}</Modal.Title>
 		          </Modal.Header>
 		          <Modal.Body>
-		            	Testo
+		            	<Table striped bordered condensed hover>
+						    <thead>
+						      <tr>
+						      	<th></th>
+						        <th>Username</th>
+						        <th>Nome</th>
+						        <th>email</th>
+						        <th>Blog</th>
+						      </tr>
+						    </thead>
+						    <tbody>
+						    	{rows}
+						    </tbody>
+						  </Table>
 		          </Modal.Body>
 		          <Modal.Footer>
 		          	<button 
