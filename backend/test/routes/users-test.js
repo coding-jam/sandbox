9eefd4dcf8d3d577d16ad6e2adb6830d195fcddc
andrea.como <andrea.como@gmail.com>
2015-08-23 00:42:34 +0200

    Added http-utils.js for testing purpose

--- backend/test/routes/users-test.js
+++ backend/test/routes/users-test.js
@@ -1,30 +1,11 @@
-var languagesAdapter = require('../../services/languages-adapter');
-var apiParams = require('../../services/api-params');
+var httpUtils = require('./http-utils');
 var expect = require('chai').expect;
 var _ = require('underscore');
-var request = require('request');
-var Q = require('q');
 
 describe('Users Routes Test suite', function () {
 
     this.timeout(100000);
     this.slow(1000000);
-
-    var options = {
-        method: "GET",
-        timeout: 1000000,
-        followRedirect: true,
-        maxRedirects: 10,
-        json: true
-    };
-
-    function getUri(uri) {
-        options.uri = 'http://localhost:8080' + apiParams.getApiPath() + uri;
-        return Q.nfcall(request, options)
-            .then(function(resp) {
-                return {header: resp[0], body: resp[1]};
-            });
-    }
 
     before(function() {
         require('../../server');
@@ -34,7 +15,7 @@ describe('Users Routes Test suite', function () {
 
         it('should return all italian region url', function (done) {
 
-            getUri('/users')
+            httpUtils.getUri('/users')
                 .then(function(resp) {
                     expect(resp.body).to.have.property('usersInLocations');
                     expect(resp.body).to.have.property('links');
