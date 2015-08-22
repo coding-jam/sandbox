bc0a5e9023099f69835d99cc78379f17f5f7135f
andrea.como <andrea.como@gmail.com>
2015-08-22 17:52:19 +0200

    Locations tests

---
+++ backend/test/locations-datasource-test.js
@@ -0,0 +1,36 @@
+var locationDs = require('../services/locations-datasource');
+var expect = require('chai').expect;
+
+describe('Locations Datasource Test suite', function () {
+
+    describe('findRegioni', function () {
+
+        it('should return italian regions from locations.js', function (done) {
+
+            locationDs.findRegioni()
+                .then(function (regions) {
+                    expect(regions).to.be.not.null;
+                    expect(regions).is.a('array');
+                    expect(regions).to.have.length(20);
+                })
+                .done(done);
+
+        });
+    });
+
+    describe('getRegioni', function () {
+
+        it('should return italian regions from regions.js', function (done) {
+
+            locationDs.findRegioni()
+                .then(function (regions) {
+                    expect(regions).to.be.not.null;
+                    expect(regions).is.a('array');
+                    expect(regions).to.have.length(20);
+                })
+                .done(done);
+
+        });
+    });
+
+});
