cea1ce03c8f570542c28f6e04241070270067269
andrea.como <andrea.como@gmail.com>
2015-09-16 00:40:55 +0200

    Connection fixed

--- backend/services/dao/mongodb/mongo-connection.js
+++ backend/services/dao/mongodb/mongo-connection.js
@@ -1,9 +1,13 @@
 var mongo = require('mongodb').MongoClient;
 
-var url = process.env.OPENSHIFT_MONGODB_DB_URL || 'http://localhost:27017/gitmap';
+var url = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost:27017/gitmap';
 
-console.log('MongoDB host: ' + process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost');
+console.log('MongoDB host: ' + (process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost'));
 
 module.exports = function() {
-    return mongo.connect(url);
+    return mongo.connect(url)
+        .catch(function (err) {
+            console.error(err);
+        });
+
 }
