348b1a2b4c22c97ac0ea4970e893e23329c32a42
albertorugnone <rugnonealbero@gmail.com>
2015-10-14 23:57:23 +0200

    added method to retrieve all districts

--- backend/services/etag-updater.js
+++ backend/services/etag-updater.js
@@ -1,23 +1,45 @@
 "use strict";
 
 var _ = require('lodash');
-var q = require('q');
+var Q = require('q');
 
-var userAdapter = require('./adapters/mongodb/users-adapter');
 
-var languages = require('./country-mappings.js').language;
+var countriesDs = require('./dao/mongodb/countries-datasource');
+var connect = require('./dao/mongodb/mongo-connection');
 
-var _getUsersByDistrict = _.curry(userAdapter.getUsersPerDistrict);
-
-var getAllUsersByDistrict = _.map(languages, _getUsersByDistrict);
-
-var _allUsers = function(){
-    return q.all(getAllUsersByDistrict);
+var toCollectionName = function (country) {
+    return country + "_users";
 }
 
 
+
+var _allUsers = function () {
+    return Q(["1"]);
+}
+
+var _allUsersToUrl = function () {
+    var q = Q(["2"]);
+    return q;
+}
+
+// _allEtafs :: Mongos.prototype.connect -> Url -> [String]
+var _allEtags = function(dbConnection, gitHubUrl) {
+    return Q([]);
+}
+
+
+var _allDistricts = function() {
+    return Q.when(connect()).then(function(db) {
+        return countriesDs.getCountriesLocations(db);
+    });
+}
+
 var etag_updater = {
-    allUsers : _allUsers
+    allUsers: _allUsers,
+    allEtags: _allEtags,
+    allUsersToUrl: _allUsersToUrl,
+    allDistrict: _allDistricts
+
 };
 
 module.exports = etag_updater;
--- backend/test/services/etag-updater-test.js
+++ backend/test/services/etag-updater-test.js
@@ -7,33 +7,97 @@
 var etagUpdater = require('../../services/etag-updater.js');
 var chai = require('chai');
 var expect = chai.expect;
+var fail = chai.fail;
 
 
-describe("Starting etag updater", function() {
-    it("updater has to be defined", function() {
+describe("Starting etag updater", function () {
+    it("updater has to be defined", function () {
         expect(etagUpdater).to.be.not.null;
     });
 
-    it("function to get all user is define", function() {
+    it("function to get all user is define", function () {
         expect(etagUpdater.allUsers).to.be.not.null;
     });
 
-    describe("if etagUpdater require all users", function() {
+});
 
-        var users;
-        beforeEach(function(done){
-           etagUpdater.allUsers()
-               .then(function(data) {
-                   users = data;
-                   done();
-               });
+describe.only("if it requires all district", function() {
+    var countries;
+    beforeEach(function (done) {
+        etagUpdater.allDistrict()
+            .then(function (data) {
+                countries = data.europe.countries;
+            }) .catch(console.error)
+            .finally(done);
+    });
 
-        });
-
-       it("receive a list of user from db", function() {
-            expect(users).to.be.not.empty;
-       }) ;
+    it("receives a list of district", function(){
+        expect(countries).to.be.not.empty;
 
     });
 
 });
+
+
+describe("if it requires all users", function () {
+
+    var users;
+    beforeEach(function (done) {
+        etagUpdater.allUsers()
+            .then(function (data) {
+                users = data;
+                users.forEach(function(user) {
+                    console.log(user.id)
+                });
+            }) .catch(console.error)
+            .finally(done);
+    });
+
+    it("receives a list of user from db", function () {
+        expect(users).to.be.not.empty;
+    });
+
+});
+
+describe("if it requires all url", function () {
+    var urls;
+
+    beforeEach(function (done) {
+        console.log("" + etagUpdater.allUsersToUrl);
+        etagUpdater.allUsersToUrl()
+            .then(function (data) {
+                urls = data;
+            })
+            .catch(console.error)
+            .finally(done);
+    });
+
+
+    it("receives a list of url", function () {
+        console.log("" + urls);
+        urls.forEach(console.log);
+        expect(urls).to.be.not.empty;
+    });
+});
+
+describe("if it requires all etags", function () {
+    var etags;
+
+    beforeEach(function (done) {
+        etagUpdater.allEtags()
+            .then(function (data) {
+                etags = data;
+            })
+            .catch(console.error)
+            .finally(done);
+    });
+
+    it("receives a list of etags", function () {
+        console.log("" + etags);
+        etags.forEach(console.log);
+        expect(etags).to.be.not.empty;
+
+    });
+
+});
+
